ff5cd4dd82a96a2a06e70ff53c0e4c095558224e
ogruiz <ogiovanniruiz@gmail.com>
2019-09-18 22:18:59 -0700

    Target MetData

--- services/censusTract.js
+++ services/censusTract.js
@@ -3,7 +3,7 @@ var CensusTract = require('../models/censustracts/censustract');
 
 const getAllCensusTracts= async(parcelDetail) =>{
     try {
-        if(parcelDetail.bounds){
+        if(parcelDetail.bounds._southWest){
             p0 = [parcelDetail.bounds._southWest.lng, parcelDetail.bounds._southWest.lat]
             p1 = [parcelDetail.bounds._southWest.lng, parcelDetail.bounds._northEast.lat]
             p2 = [parcelDetail.bounds._northEast.lng, parcelDetail.bounds._northEast.lat]
@@ -13,6 +13,10 @@ const getAllCensusTracts= async(parcelDetail) =>{
             var arrayCoords = [p0, p1, p2, p3, p4]
 
             var tracts = await CensusTract.find({"properties.location": {$geoWithin: { $geometry: {type: "Polygon" , coordinates: [arrayCoords] }}}})
+            return tracts
+        } else{
+            //var tracts = await CensusTract.find({"properties.lrs": {$gte: 27.0}})
+            var tracts = await CensusTract.find()
             return tracts
         }
 
