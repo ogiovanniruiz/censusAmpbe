877809fa044d7f94c0add10651bccf2ac311f2b3
“Rick” <roderick@roderickhunter.com>
2020-01-27 20:12:34 -0800

    Reports update with flat table

--- routes/reports.js
+++ routes/reports.js
@@ -3,5 +3,8 @@ var router = express.Router();
 var reportController = require('../controllers/report.js')
 
 router.post('/updateReport', reportController.updateReport);
+router.post('/getCanvassSummaryReport', reportController.getCanvassSummaryReport);
+router.post('/getPetitionSummaryReport', reportController.getPetitionSummaryReport);
+router.post('/getOverallSummaryReport', reportController.getOverallSummaryReport);
 
 module.exports = router;
